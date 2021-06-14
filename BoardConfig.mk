0404e9c9c92dc3409bc97ae203b136c9e95c0da3
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-06-14 07:56:54 +0200

    panther: set vendor spl
    Change-Id: I0559a2a4943713f025cdb70a7651709884557b11

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-11-05
+VENDOR_SECURITY_PATCH := 2021-03-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
