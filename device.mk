37e28a2252f1bc57960a1322581ddfd08cd0e12e
LuK1337 <priv.luk@gmail.com>
2021-12-14 11:45:20 +0100

    panther: Disable APEX compression
    Kernel panics on linux-4.9 + ext4.
    
    Change-Id: I3ae1322880286a7edf8e53e6f602618d129bba91

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+PRODUCT_COMPRESSED_APEX := false
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nokia/PAN_sprout/PAN_sprout-vendor.mk)
