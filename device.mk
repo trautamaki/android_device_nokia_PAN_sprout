d7b6626e496737207a61fca0eeac8e6acdc59feb
LuK1337 <priv.luk@gmail.com>
2021-12-15 17:58:52 +0100

    panther: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Change-Id: Iae740ef18505b7f2bb01fe54d45810e72fd521e9

--- device.mk
+++ device.mk
@@ -75,12 +75,8 @@ PRODUCT_PACKAGES += \
     tinymix
 
 # ANT+
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
+    AntHalService
 
 # Bluetooth
 PRODUCT_PACKAGES += \
