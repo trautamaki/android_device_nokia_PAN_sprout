c39ce9332f835ecbf276d1271d0f4d0ed30bb96f
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:31:43 +0000

    panther: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I1af8461f8ad82afbb21b43dd6968e824ea89f5d6

--- device.mk
+++ device.mk
@@ -67,7 +67,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudioalsa \
